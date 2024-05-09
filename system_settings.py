e0b9d57e1e343555b0835de41225c0a9407dcfa6
2-adic <two.adic@gmail.com>
2024-05-09 17:13:39 -0400

    Added: logic for Windows to apply transparency to the application background.

--- system_settings.py
+++ system_settings.py
@@ -38,4 +38,30 @@ class OperatingSystem:
         else:
             return False  # operating system not recognized
 
+    def enable_blur(self, window):
+        if self.system_name == 'Windows':
 
+            hwnd = window.windowHandle().winId().__int__()  # Obtain the HWND
+
+            class ACCENT_POLICY(ctypes.Structure):
+                _fields_ = [("AccentState", ctypes.c_int),
+                            ("AccentFlags", ctypes.c_int),
+                            ("GradientColor", ctypes.c_uint),
+                            ("AnimationId", ctypes.c_uint)]
+
+            class WINDOW_COMPOSITION_ATTRIBUTE_DATA(ctypes.Structure):
+                _fields_ = [("Attribute", ctypes.c_int),
+                            ("Data", ctypes.POINTER(ACCENT_POLICY)),
+                            ("SizeOfData", ctypes.c_size_t)]
+
+            accent = ACCENT_POLICY()
+            accent.AccentState = 3  # ACCENT_ENABLE_BLURBEHIND=3
+            accent.AccentFlags = 2  # draw all borders=2
+            accent.GradientColor = 0  # transparent gradient color
+            accent.AnimationId = 0  # no animation
+
+            data = WINDOW_COMPOSITION_ATTRIBUTE_DATA()
+            data.Attribute = 19  # WCA_ACCENT_POLICY
+            data.Data = ctypes.pointer(accent)
+            data.SizeOfData = ctypes.sizeof(accent)
+            ctypes.windll.user32.SetWindowCompositionAttribute(hwnd, ctypes.byref(data))
