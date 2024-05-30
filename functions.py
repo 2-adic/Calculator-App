e2f6074d7e4711ee1155e7757d6b79107b3fbf01
2-adic <two.adic@gmail.com>
2024-05-30 04:04:01 -0400

    Added: moved a lot of solution functionallity to this file.

--- functions.py
+++ functions.py
@@ -2,39 +2,105 @@ import sympy as sy
 import symbols
 import str_format as form
 from latex import convert_render_latex
+from files import file_path
 
 
 class Solve:
-    def __init__(self, expression: str):
+    def __init__(self, expression: str, render_color: tuple[int, int, int], render_dpi: int):
 
         self.funct = {
-            0: self.integrate, 1: self.sin, 2: self.cos
+            '0': self.integrate, '1': self.sin, '2': self.cos
         }
 
         self.constant_counter = 0  # keeps track of the amount of constants used
         self.expression = expression
         self.expression = self.expression.replace(' ', '')  # removes spaces to fix formatting issues
 
-        self.expression_solved = self.solve(self.expression)
+        self.error = None  # saves the reason for an error
+        self.answer_exact = None
+        self.answer_approximate = None
+
+        self.expression_solved = self.solve(self.expression)  # solves the expression
+        self.exact()  # turns the solution into its exact form
+        self.approximate()  # turns the solution into its approximate form
+        self.render(render_color, render_dpi)  # renders the image of the exact and approximate solutions
+        # self.final_formatting()  # formats the exact and approximate answer forms
+
+        if self.error is not None:
+            print(f'Error: {self.error}')
 
     def __str__(self):
         return self.expression_solved
 
-    def render(self, color: tuple[int, int, int] = (255, 255, 255)):
+    def render(self, color: tuple[int, int, int] = (255, 255, 255), dpi: int = 300):
         """
-        Renders an image of the solved expression.
+        Renders images of the solved expression.
 
         :param color: The color of the rendered text.
+        :param dpi: The
         """
 
-        convert_render_latex(self.expression_solved, color, constant_amount=self.constant_counter)
+        exact = file_path('latex_exact.png')
+        approximate = file_path('latex_approximate.png')
+
+        convert_render_latex(self.answer_exact, color, dpi, exact, self.constant_counter)
+        convert_render_latex(self.answer_approximate, color, dpi, approximate, self.constant_counter)
 
     def print(self):
         """
         Prints the initial expression, and the solved expression.
         """
 
-        print(f'{self.expression} = {self.expression_solved}')
+        expression = self.expression  # copies the expression to save the original
+        for key in list(self.funct.keys()):
+            expression = expression.replace(f'§{key}', symbols.accepted_functions[int(key)])
+
+        print(f'{expression} = {self.expression_solved}')
+
+    def exact(self):
+        """
+        Turns the answer into its exact form.
+        """
+
+        try:
+            self.answer_exact = sy.simplify(self.expression_solved)
+
+        except Exception as e:
+            self.answer_exact = 'Error'
+            self.error = e
+
+    def get_exact(self) -> str:
+        """
+        Returns the answer in its exact form.
+        """
+
+        return self.answer_exact
+
+    def approximate(self):
+        """
+        Turns the answer into its approximate form.
+        """
+
+        try:
+            self.answer_approximate = sy.N(self.expression_solved)
+
+        except Exception as e:
+            self.answer_approximate = 'Error'
+            self.error = e
+
+    def get_approximate(self) -> str:
+        """
+        Returns the answer in its approximate form.
+        """
+        return self.answer_approximate
+
+    def final_formatting(self):
+        """
+        Performs some final formatting to the answer in its exact and approximate form.
+        """
+
+        self.answer_exact = self.answer_exact.replace('**', '^')
+        self.answer_approximate = self.answer_approximate.replace('**', '^')
 
     def solve(self, string: str) -> str:
         """
@@ -45,45 +111,25 @@ class Solve:
         """
 
         # solves a function with all of its inner functions and loops until all functions are solved
-        while form.contains(string, symbols.accepted_functions):
+        while '§' in string:
             string = self.function_check(string)
 
         return string
 
-    def function_check(self, f: str) -> str:
+    def function_check(self, expression: str) -> str:
         """
         Checks for math functions, solves them, and puts the answer back where the function was.
 
-        :param f: The expression with a possible function to be solved.
+        :param expression: The expression with a possible function to be solved.
         :return: The expression with the solved function.
         """
 
-        # may not need to check for this
-        if not form.contains(f, symbols.accepted_functions):
-            return f
+        function_id, parameters, index_start, index_end = form.get_function_parameters(expression)
+        ans = self.funct[function_id](*parameters)
 
-        # need to redo this to make it easier to work with
-        function = form.find_substring_index(f, symbols.accepted_functions)
-        info = form.get_elements_in_bracket(f, function[1])
-        parameter = info[0]
+        expression = form.replace_substring(expression, index_start - len(f'§{function_id}'), index_end, ans)
 
-        if function[0] == 'Integrate':
-            ans = self.integrate(parameter[0], parameter[1])
-
-            f = form.replace_substring(f, function[1] - len('Integrate'), info[1], ans)
-            return f
-
-        elif function[0] == 'Sin':
-            ans = self.sin(parameter[0])
-
-            f = form.replace_substring(f, function[1] - len('Sin'), info[1], ans)
-            return f
-
-        elif function[0] == 'Cos':
-            ans = self.cos(parameter[0])
-
-            f = form.replace_substring(f, function[1] - len('Cos'), info[1], ans)
-            return f
+        return expression
 
     def integrate(self, f: str, x: str) -> str:  # 'Integrate[f, x]' -> ∫(f)dx
         """
@@ -98,8 +144,8 @@ class Solve:
         if x not in symbols.accepted_variables:
             raise 'Error: Second parameter not formatted correctly'
 
-        # solves other functions inside the integral before solving the integral
-        if form.contains(f, symbols.accepted_functions):
+        # solves other functions inside the function before solving the function
+        while '§' in f:
             f = self.function_check(f)
 
         # adds a unique constant
