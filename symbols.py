a8ee9f6924fe5be1cf108d0e177993643da23b28
2-adic <two.adic@gmail.com>
2024-06-14 03:20:38 -0400

    Changed: moved sympy symbols to this file, added functions for the user to copy.

--- symbols.py
+++ symbols.py
@@ -1,19 +1,3 @@
-def generate_function_dictionary(dictionary_name: str) -> None:
-    """
-    Used to replace the function dictionary when more functions are added.
-    """
-
-    string = f'{dictionary_name} = {{\n    '
-
-    for i, function in enumerate(accepted_functions):
-        string += f"'{i}': self.{function.lower()}, "
-
-    string = string[:-2]
-    string += '\n}'
-
-    print(string)
-
-
 def get_constant_values(dictionary: dict, digits: int) -> dict:
     """
     Sets the amount of digits for the constants.
@@ -37,17 +21,17 @@ def get_constant_values(dictionary: dict, digits: int) -> dict:
     # adds each constant value to the new dictionary with the specified amount of digits
     ans = {}
     for key in sorted(dictionary.keys()):
-        ans[key] = dictionary[key][:digits]
+        ans[key] = dictionary[key][1][:digits]
 
     return ans
 
 
 constants = {
-    'i': 'i',
-    'e': '2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274',
-    'π': '3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679',
-    'φ': '1.6180339887498948482045868343656381177203091798057628621354486227052604628189024497072072041893911374',
-    'γ': '0.5772156649015328606065120900824024310421593359399235988057672348848677267776646709369470632917467495'
+    'i': ('I', 'i'),
+    'e': ('E',              '2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274'),
+    'π': ('pi',             '3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679'),
+    'φ': ('GoldenRatio',    '1.6180339887498948482045868343656381177203091798057628621354486227052604628189024497072072041893911374'),
+    'γ': ('EulerGamma',     '0.5772156649015328606065120900824024310421593359399235988057672348848677267776646709369470632917467495')
 }
 
 accepted_constants = sorted(constants.keys())
@@ -59,13 +43,16 @@ accepted_variables = sorted([
 
 accepted_numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-copy_symbols = ['π', 'φ', 'γ', 'θ']
-
 accepted_functions = [
     'Integrate',
     'Ln',
     'Sin',
     'Cos',
+]
+
+copy_notation = [
+    ['π', 'φ', 'γ', 'θ'],
+    accepted_functions
 ]
 
 subscript = {
@@ -75,4 +62,5 @@ subscript = {
     '(': '₍', ')': '₎'
 }
 
-# generate_function_dictionary('self.funct')
+constant_values = get_constant_values(constants, 20)
+constant_preview = get_constant_values(constants, 2)
