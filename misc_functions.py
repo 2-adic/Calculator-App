9d30836bc2c3b338067315bc38194ab07296c7db
2-adic <two.adic@gmail.com>
2024-07-17 03:31:12 -0400

    Added: a new function.

--- misc_functions.py
+++ misc_functions.py
@@ -24,3 +24,25 @@ def get_line_edit_key(dictionary: dict, line_edit: QLineEdit) -> str:
     for i, key in enumerate(sorted(dictionary.keys())):
         if dictionary[key][1] == line_edit:
             return key
+
+
+def is_all_int(array: tuple[str, ...]) -> bool:
+    """
+    Checks if all the elements in a list are ints or not.
+
+    Assumes all elements are in the form: _ or _.__
+    """
+
+    for element in array:
+        if '/' in element:  # number is a fraction (not an int since fractions are simplified)
+            return False
+
+        if '.' in element:  # if a non 0 appears after a decimal point, it is not an int
+
+            element = element[element.find('.') + 1:]
+
+            for char in element:
+                if char != '0':
+                    return False
+
+    return True
