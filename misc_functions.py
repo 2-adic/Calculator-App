a62c967fc5a9e6368b80aea3994446f552b6ccee
2-adic <two.adic@gmail.com>
2024-07-23 02:21:18 -0400

    Added: functions to help with testing color updating.

--- misc_functions.py
+++ misc_functions.py
@@ -1,4 +1,5 @@
 from PyQt6.QtWidgets import QLineEdit
+from random import randint
 
 
 def get_position_change(key_list_prev: list, key_list_curr: list, string: str) -> int:
@@ -52,3 +53,78 @@ def get_constant_values(dictionary: dict, digits: int) -> dict:
         ans[key] = dictionary[key][1][:digits]
 
     return ans
+
+
+def test_colors(settings) -> None:
+    """
+    Changes the colors of the windows to test if all colors are changing.
+    """
+
+    settings.color_background = 54, 92, 46
+    settings.color_text = 177, 58, 58
+    settings.color_text_highlight_active = 168, 171, 45
+    settings.color_text_highlight_inactive = 163, 71, 45
+    settings.color_text_secondary = 130, 135, 162
+    settings.color_title_bar = 81, 40, 48
+    settings.color_title_bar_text = 175, 178, 61
+    settings.color_title_bar_button_hover = 40, 53, 141
+    settings.color_title_bar_button_press = 182, 46, 119
+    settings.color_title_bar_button_exit_hover = 181, 96, 133
+    settings.color_title_bar_button_exit_press = 47, 137, 88
+    settings.color_box_background = 58, 88, 104
+    settings.color_box_hover = 148, 66, 155
+    settings.color_box_selected = 139, 164, 32
+    settings.color_box_border = 150, 196, 188
+    settings.color_line_primary = 82, 54, 66
+    settings.color_line_secondary = 20, 140, 51
+    settings.color_scrollbar_background = 134, 51, 75
+
+    settings.color_latex = settings.color_text
+
+
+def generate_new_colors() -> None:
+    """
+    Prints out new random rgb values to paste into the test_colors function.
+    """
+
+    color_min = 20
+    color_max = 200
+
+    string = (
+        '''
+        settings.color_background
+        settings.color_text
+        settings.color_text_highlight_active
+        settings.color_text_highlight_inactive
+        settings.color_text_secondary
+        settings.color_title_bar
+        settings.color_title_bar_text
+        settings.color_title_bar_button_hover
+        settings.color_title_bar_button_press
+        settings.color_title_bar_button_exit_hover
+        settings.color_title_bar_button_exit_press
+        settings.color_box_background
+        settings.color_box_hover
+        settings.color_box_selected
+        settings.color_box_border
+        settings.color_line_primary
+        settings.color_line_secondary
+        settings.color_scrollbar_background
+        '''
+    )
+
+    string = string.replace(' ', '')
+
+    lines = string.split('\n')
+    lines.pop(0)
+    lines.pop(-1)
+
+    result = ''
+    for line in lines:
+        line += f' = {randint(color_min, color_max)}, {randint(color_min, color_max)}, {randint(color_min, color_max)}'
+        result += line + '\n'
+
+    print(result)
+
+
+# generate_new_colors()
