fc5ece5fbcbb6beef9d0331058dea2d29f9dec98
2-adic <two.adic@gmail.com>
2024-08-06 20:16:41 -0400

    Added: text box elements with a custom caret color.

--- elements.py
+++ elements.py
@@ -1,6 +1,6 @@
-from PyQt6.QtWidgets import QPushButton, QLabel, QVBoxLayout
-from PyQt6.QtGui import QIcon
-from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtWidgets import QPushButton, QLabel, QVBoxLayout, QProxyStyle, QStyle, QLineEdit, QApplication, QPlainTextEdit
+from PyQt6.QtGui import QIcon, QColor, QPainter
+from PyQt6.QtCore import Qt, QSize, QTimer, QRect, pyqtProperty
 
 
 class WrapTextButton:
@@ -70,3 +70,185 @@ class WrapTextButton:
 
     def resize(self, w: int, h: int) -> None:
         self.__button.resize(w, h)
+
+
+class CustomCaretLineEditStyle(QProxyStyle):
+    def pixelMetric(self, pm, opt=None, widget=None):
+        if (
+            pm == QStyle.PixelMetric.PM_TextCursorWidth and opt
+            and isinstance(opt.styleObject, CustomCaretLineEdit)
+            and opt.styleObject.property('customCaret')
+        ):
+            return 0
+        return super().pixelMetric(pm, opt, widget)
+
+
+class CustomCaretLineEdit(QLineEdit):
+    # default values
+    __caret_size = -1
+    __caret_color = QColor(255, 255, 255)  # the active color of the caret
+    __background_color = QColor(0, 0, 0)  # background color to hide the caret
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setStyle(CustomCaretLineEditStyle())
+        self.__blink_timer = QTimer(self, timeout=self.__toggle_caret_color, interval=QApplication.styleHints().cursorFlashTime() // 2)
+        self.__current_color = self.__background_color
+
+    @pyqtProperty(int)
+    def caret_size(self):
+        return self.__caret_size
+
+    @caret_size.setter
+    def caret_size(self, size):
+        if size < 0:
+            size = -1
+        if self.__caret_size != size:
+            self.__caret_size = size
+            self.update()
+
+    @pyqtProperty(QColor)
+    def caret_color(self):
+        return self.__caret_color
+
+    @caret_color.setter
+    def caret_color(self, color: QColor):
+        self.__caret_color = color
+        self.update(self.cursorRect())
+
+    @pyqtProperty(QColor)
+    def background_color(self):
+        return self.__background_color
+
+    @background_color.setter
+    def background_color(self, color: QColor):
+        self.__background_color = color
+        self.__current_color = self.__background_color
+        self.update(self.cursorRect())
+
+    def __toggle_caret_color(self):
+        # toggles between the caret color and the background color
+        if self.__current_color == self.__background_color:
+            self.__current_color = self.__caret_color  # set to visible color
+        else:
+            self.__current_color = self.__background_color  # set to background color to hide
+        self.update(self.cursorRect())
+
+    def focusInEvent(self, event):
+        super().focusInEvent(event)
+
+        self.__current_color = self.__caret_color  # makes caret visible immediately
+        self.update(self.cursorRect())  # updates the caret to show the color
+
+        self.__blink_timer.start()
+
+    def focusOutEvent(self, event):
+        super().focusOutEvent(event)
+        self.__blink_timer.stop()
+        self.__current_color = self.__background_color  # makes caret invisible on focus loss
+        self.update()
+
+    def keyPressEvent(self, event):
+        super().keyPressEvent(event)
+        self.__current_color = self.__caret_color   # makes caret visible after a key press
+        self.__blink_timer.start()
+
+    def paintEvent(self, event):
+        super().paintEvent(event)
+        if self.__caret_size > 0:
+            qp = QPainter(self)
+            full_rect = self.cursorRect()
+            x = full_rect.x() + round(full_rect.width() / 2) - round(self.__caret_size / 2)
+            cr = QRect(x, full_rect.y(), max(1, self.__caret_size), self.fontMetrics().height())
+            qp.fillRect(cr, self.__current_color)
+
+
+class CustomCaretTextEditStyle(QProxyStyle):
+    def pixelMetric(self, pm, opt=None, widget=None):
+        if (
+            pm == QStyle.PixelMetric.PM_TextCursorWidth and opt
+            and isinstance(opt.styleObject, CustomCaretTextEdit)
+            and opt.styleObject.property('customCaret')
+        ):
+            return 0
+        return super().pixelMetric(pm, opt, widget)
+
+
+class CustomCaretTextEdit(QPlainTextEdit):
+    # default values
+    __caret_size = -1
+    __caret_color = QColor(255, 255, 255)  # the active color of the caret
+    __background_color = QColor(0, 0, 0)  # background color to hide the caret
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setStyle(CustomCaretTextEditStyle())
+        self.__blink_timer = QTimer(self, timeout=self.__toggle_caret_color, interval=QApplication.styleHints().cursorFlashTime() // 2)
+        self.__current_color = self.__background_color
+
+    @pyqtProperty(int)
+    def caret_size(self):
+        return self.__caret_size
+
+    @caret_size.setter
+    def caret_size(self, size):
+        if size < 0:
+            size = -1
+        if self.__caret_size != size:
+            self.__caret_size = size
+            self.update()
+
+    @pyqtProperty(QColor)
+    def caret_color(self):
+        return self.__caret_color
+
+    @caret_color.setter
+    def caret_color(self, color: QColor):
+        self.__caret_color = color
+        self.update(self.cursorRect())
+
+    @pyqtProperty(QColor)
+    def background_color(self):
+        return self.__background_color
+
+    @background_color.setter
+    def background_color(self, color: QColor):
+        self.__background_color = color
+        self.__current_color = self.__background_color
+        self.update(self.cursorRect())
+
+    def __toggle_caret_color(self):
+        # toggles between the caret color and the background color
+        if self.__current_color == self.__background_color:
+            self.__current_color = self.__caret_color  # sets to visible color
+        else:
+            self.__current_color = self.__background_color  # sets to background color to hide
+        self.update(self.cursorRect())
+
+    def focusInEvent(self, event):
+        super().focusInEvent(event)
+
+        self.__current_color = self.__caret_color  # makes caret visible immediately
+        self.update(self.cursorRect())  # updates the caret to show the color
+
+        self.__blink_timer.start()
+
+    def focusOutEvent(self, event):
+        super().focusOutEvent(event)
+        self.__blink_timer.stop()
+        self.__current_color = self.__background_color  # makes caret invisible on focus loss
+        self.update()
+
+    def keyPressEvent(self, event):
+        super().keyPressEvent(event)
+        self.__current_color = self.__caret_color   # makes caret visible after a key press
+        self.__blink_timer.start()
+
+    def paintEvent(self, event):
+        super().paintEvent(event)
+        if self.__caret_size > 0:
+            qp = QPainter(self.viewport())
+            full_rect = self.cursorRect()
+            x = full_rect.x() + round(full_rect.width() / 2) - round(self.__caret_size / 2)
+            cr = QRect(x, full_rect.y(), max(1, self.__caret_size), self.fontMetrics().height())
+            qp.fillRect(cr, self.__current_color)
